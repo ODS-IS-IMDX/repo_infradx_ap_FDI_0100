ed8ab95e232d0b8bdb4d0066d2df262df5f608f2
Junki.Mogi <Junki.Mogi.bp@jp.nttdata.com>
2026-02-06 16:34:35 +0900

    結合試験故障処理表_項番2の修正対応

--- App/functions/FDI_0100_tblExportEquipmentMasterTo3d.py
+++ App/functions/FDI_0100_tblExportEquipmentMasterTo3d.py
@@ -80,12 +80,12 @@ def check_required_scripts_exist():
 
 # 1. 入力値チェック
 def validate_layer_ids(layer_ids):
-    for layer_id in layer_ids:
-        # レイヤIDの必須チェック
-        if not layer_id:
-            logger.error("BPE0018", "レイヤID")
-            logger.process_error_end()
+    # レイヤIDの必須チェック
+    if not layer_ids:
+        logger.error("BPE0018", "レイヤID")
+        logger.process_error_end()
 
+    for layer_id in layer_ids:
         # レイヤIDが、半角英数字とアンダースコアのみで構成されているか
         if not Validations.is_alnum_underscore(layer_id):
             logger.error("BPE0019", "レイヤID", layer_id)
@@ -367,8 +367,8 @@ def create_or_refresh_matview(matview_no_list, matview_yes_list, layer_info_map)
         db_connection = Database.get_refdb_connection(db_host, logger)
         for query in ddl_queries:
             try:
-                Database.execute_query(
-                    db_connection, logger, query, commit=False, raise_exception=True
+                Database.execute_query_no_commit(
+                    db_connection, logger, query, raise_exception=True
                 )
             except Exception:
                 process_code = Constants.RETURNCODE_WARNING
